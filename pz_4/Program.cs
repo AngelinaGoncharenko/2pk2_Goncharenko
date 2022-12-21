f2c7cb03c80da6eb247fa6782076bea54f637630
Ангелина <114383142+AngelinaGoncharenko@users.noreply.github.com>
2022-12-21 09:00:02 +0500

    4

---
+++ pz_4/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace pz_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            switch (k)
+            {
+                case 1:
+                    Console.WriteLine("Привет, первоклассник!!!");
+                    break;
+                case 2:
+                    Console.WriteLine("Привет, второклассник!!!");
+                    break;
+                case 3:
+                    Console.WriteLine("Привет, третьеклассник!!!");
+                    break;
+                case 4:
+                    Console.WriteLine("Привет, четвероклассник!!!");
+                    break;
+                case 5:
+                    Console.WriteLine("Привет, пятиклассник!!!");
+                    break;
+                case 6:
+                    Console.WriteLine("Привет, шестиклассник!!!");
+                    break;
+                case 7:
+                    Console.WriteLine("Привет, семиклассник!!!");
+                    break;
+                case 8:
+                    Console.WriteLine("Привет, восьмиклассник!!!");
+                    break;
+                case 9:
+                    Console.WriteLine("Привет, девятиклассник!!!");
+                    break;
+                case 10:
+                    Console.WriteLine("Привет, десятиклассник!!!");
+                    break;
+                case 11:
+                    Console.WriteLine("Привет, одиннадцатиклассник!!!");
+                    break;
+            }
+    }
+}
