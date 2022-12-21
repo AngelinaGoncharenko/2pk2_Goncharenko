4452662596611892fa72a9cb853403a20c235e9b
Ангелина <114383142+AngelinaGoncharenko@users.noreply.github.com>
2022-12-21 09:16:56 +0500

    calculator(Console)

---
+++ pz_2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace pz_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пример(допустимые операции: +, -, *, /)");
+
+            int a = Convert.ToInt32(Console.Read());
+            char c = Convert.ToChar(Console.Read());
+            int b = Convert.ToInt32(Console.Read());
+            if (c == '-')
+            {
+                Console.WriteLine("=" + (a - b));
+            }
+            else if (c == '+')
+            {
+                Console.WriteLine("=" + (a + b));
+            }
+            else if (c == '*')
+            {
+                Console.WriteLine("=" + (a * b));
+            }
+            else if (c == '/') ;
+            {
+                Console.WriteLine("=" + (a / b));
+            }
+
+        }
+    }
+}
