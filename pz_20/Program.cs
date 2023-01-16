a719173ff761d3321520f327c19f6dd17dce8d59
Гончаренко Ангелина <angelinagoncharenko5@outlook.com>
2023-01-16 16:03:32 +0500

    метод меняющий регистр

---
+++ pz_20/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace pz_20
+{
+    internal class Program
+    {
+        static string GetReverse(string txt)
+        {
+            string result = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (Char.IsUpper(txt[i]))
+                {
+                    result += Char.ToLower(txt[i]);
+                }
+                else if (Char.IsLower(txt[i]))
+                {
+                    result += Char.ToUpper(txt[i]);
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string txt = Console.ReadLine();
+           
+            Console.WriteLine(GetReverse(txt));
+        }
+    }
+}
