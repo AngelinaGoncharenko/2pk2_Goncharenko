f36ec7118439f52c493d67ab260b2a404550493a
Гончаренко Ангелина <angelinagoncharenko5@outlook.com>
2023-01-26 13:27:23 +0500

    ооп, реализация свойств

---
+++ Class_ageEmployee/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_ageEmployee
+{
+    internal class Person
+    {
+        int age = 1;
+        public virtual int Age
+        {
+            get => age; 
+            set { if (value > 0 && value < 110) age = value; } 
+        }
+        public string Name { get; set; }
+        public Person (string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public virtual void Print() => Console.WriteLine(Name, Age);
+    }
+}
---
+++ ООП_свойства/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ООП_свойства
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee employee = new Employee("Angelina","OOO AVANGARD");
+            employee.Print();
+        }
+    }
+}
