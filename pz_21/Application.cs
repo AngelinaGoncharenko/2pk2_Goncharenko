db63e03e86c3b5f3a3330e722171830bea76f43f
Ангелина <114383142+AngelinaGoncharenko@users.noreply.github.com>
2023-02-02 21:48:20 +0500

    заявка на заправку картриджа как сущность в приложении.

---
+++ pz_21/Application.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Application
+    {
+        public int ID { get; set; }
+        public string Customer { get; set; }
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public Application(int id, string customer, string vendor, string model)
+        {
+            ID = id;
+            Customer = customer;
+            Vendor = vendor;
+            Model = model;
+
+        }
+
+        public void GetApplicationInfo()
+        {
+            Console.WriteLine($"ID: {ID}\n Рrinter model: {Model}\n Vendor: {Vendor}\n Customer: {Customer}");
+        }
+    }
+}
---
+++ pz_21/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application C24D56 = new Application(89565743, "Niсk", "USA", "C24D56");
+            C24D56.GetApplicationInfo();
+        }
+    }
+}
