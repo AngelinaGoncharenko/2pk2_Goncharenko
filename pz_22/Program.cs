a88561d5fa28c837542c17023907249d89cfedb1
Ангелина <114383142+AngelinaGoncharenko@users.noreply.github.com>
2023-02-20 11:49:08 +0500

    .

--- pz_22/Program.cs
+++ pz_22/Program.cs
@@ -6,7 +6,7 @@ namespace pz_22
     {
         static void Main(string[] args)
         {
-            Application C24D56 = new Application(89565743, "Niсk","USA",  "C24D56", 2);
+            Application C24D56 = new Application(89565743, "Niсk","USA",  "C24D56", 25);
             C24D56.GetApplicationInfo();
             C24D56.UsedToner();
             Application C24D57 = new Application(89565744, "Niсk", "USA", "C24D57", 30);
