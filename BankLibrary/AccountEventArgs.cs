376e8b03f1e1ebe3bfc5afd84128c554b8a003b5
Ангелина <114383142+AngelinaGoncharenko@users.noreply.github.com>
2023-05-02 11:02:45 +0500

    pz_29

---
+++ BankLibrary/AccountEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+
+    }
+}
