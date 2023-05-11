5400824a9b4192df87c2f20f43c66836bd9a65cb
Ангелина <114383142+AngelinaGoncharenko@users.noreply.github.com>
2023-05-11 13:25:59 +0500

    pz_29

---
+++ BankWork(pz_29)/OpenWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BankWork_pz_29_
+{
+    /// <summary>
+    /// Логика взаимодействия для OpenWindow.xaml
+    /// </summary>
+    public partial class OpenWindow : Window
+    {
+        public OpenWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DemandItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
